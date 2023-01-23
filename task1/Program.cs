8ab04b7bd1b58933b805050c050968e9c30c2830
Zhenya <wowkylgame@gmail.com>
2023-01-23 18:16:11 +0300

    1 task complete

---
+++ task1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+System.Console.WriteLine("Введите число");;
+int num = Convert.ToInt32(Console.ReadLine());
+if (num == 6 || num == 7)
+{
+    System.Console.WriteLine("Выходной");;
+}
+else
+{
+System.Console.WriteLine("Будний");
+}
