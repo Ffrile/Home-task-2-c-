89f0e262b52d9f733d84891d1f6c442e20f49450
Zhenya <wowkylgame@gmail.com>
2023-01-23 18:16:20 +0300

    2 task complete

---
+++ task2/Program.cs
@@ -0,0 +1,37 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+// что третьей цифры нет.
+
+System.Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num < 100)
+{
+    System.Console.WriteLine("нет третьего числа");
+}
+else
+{
+    while (num >= 1000)
+    {
+        num = num / 10;
+    }
+    System.Console.WriteLine(num % 10);
+}
+
+
+
+
+// int count = 1;
+// int num2 = num;
+// while ((num2 / 10) > 0)
+// {
+//     count++;
+//     num2 = num2 / 10;
+// }
+// if (count > 2)
+// {
+//     num2 = (num / Convert.ToInt32(Math.Pow(10, (count - 3))) % 10);
+//     System.Console.WriteLine(num2);
+// }
+// else
+// {
+//     System.Console.WriteLine("нет третьего числа");
+// }
